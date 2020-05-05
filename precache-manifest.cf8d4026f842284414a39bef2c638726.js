9a39011e38b814ddadbbe1f07e0d652264e0f3cd
Julian Cuentas <juliancuentas@hotmail.com>
2020-05-05 18:50:06 -0300

    Updates

---
+++ precache-manifest.cf8d4026f842284414a39bef2c638726.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cf7ab86dabd915ac1a6e951c3aa1f3",
+    "url": "/miportfolio/index.html"
+  },
+  {
+    "revision": "403fb4f265e8aa872781",
+    "url": "/miportfolio/static/css/main.5924adbe.chunk.css"
+  },
+  {
+    "revision": "ea0c3c1244dfdfafb967",
+    "url": "/miportfolio/static/js/2.b369004f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miportfolio/static/js/2.b369004f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403fb4f265e8aa872781",
+    "url": "/miportfolio/static/js/main.9e2ef1de.chunk.js"
+  },
+  {
+    "revision": "7e162b6514aec6456d6d",
+    "url": "/miportfolio/static/js/runtime-main.7baf3987.js"
+  }
+]);
